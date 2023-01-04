17b15e9adf8fb23d09990d0db9c0c51cdd747481
razmannazli <70215211+razmannazli@users.noreply.github.com>
2023-01-05 01:16:19 +0800

    fix issue

--- src/com/razman/Main.java
+++ src/com/razman/Main.java
@@ -5,109 +5,37 @@ import java.io.File;
 import java.io.FileReader;
 import java.io.IOException;
 import java.util.*;
-import java.util.concurrent.atomic.AtomicBoolean;
-import java.util.concurrent.atomic.AtomicReference;
 
 public class Main
 {
-
     public static void main(String[] args) throws IOException {
 
         String st;
         ArrayList<Person> allPersonList = new ArrayList<>();
-        HashMap<String,  String> topPersonList = new HashMap<>();
         HashMap<String, ArrayList<Person>> parentGroupMap = new HashMap<>();
+        HashMap<String,  String> topPersonList = new HashMap<>();
+        HashMap<String, ArrayList<Person>> nameMap = new HashMap<>();
+        ArrayList<String> existedList = new ArrayList<>();
+        ArrayList<String> sortedTopPerson = new ArrayList<>();
 
-        File file = new File("C:\\Users\\Razman-NB\\Desktop\\inputtest.txt");
+        File file = new File("inputtest.txt");
         BufferedReader br = new BufferedReader(new FileReader(file));
 
         int count = 1;
         while ((st = br.readLine()) != null) {
             if (count > 1) {
                 String words[] = st.split(",");
-                Person p = new Person(words[0], words[1], words[2], words[3]);
-                allPersonList.add(p);
+                if (words.length == 4 && words[0] != "" && words[1] != "" && words[2] != "" && words[3] != "") {
+                    Person p = new Person(words[0], words[1], words[2], words[3]);
+                    allPersonList.add(p);
+                }
             }
             count++;
         }
 
-        // Grouping by parentID
-        for (int i = 0; i < allPersonList.size(); i++) {
-            Person person = allPersonList.get(i);
-            boolean topParent = true;
-            for (int j = 0; j < allPersonList.size(); j++) {
-                Person list2 = allPersonList.get(j);
-                if (person.getParentID().equals(list2.getRecordID())) {
-                    if (parentGroupMap.containsKey(person.getParentID())) {
-                        ArrayList<Person> temp = parentGroupMap.get(person.getParentID());
-                        boolean samePerson = false;
-                        for (int k = 0; k < temp.size(); k++) {
-                            if (person.getName().equals(temp.get(k).getName())) {
-                                samePerson = true;
-                            }
-                        }
-                        if (samePerson == false) {
-                            temp.add(person);
-                            parentGroupMap.put(person.getParentID(), temp);
-                        }
-                    } else {
-                        ArrayList<Person> temp = new ArrayList<Person>();
-                        temp.add(person);
-                        parentGroupMap.put(person.getParentID(), temp);
-                    }
-                    topParent = false;
-                }
-            }
+        PersonManager.groupPersonByParentID(allPersonList, parentGroupMap, topPersonList);
 
-            ArrayList<Person> temp = new ArrayList<Person>();
-            if (topParent) {
-                if (!parentGroupMap.containsKey(person.getParentID())) {
-                    parentGroupMap.put(person.getParentID(), temp);
-                topPersonList.put(person.getRecordID(), person.getName());
-                }
-            }
-        }
-
-
-
-
-        HashMap<String, ArrayList<Person>> nameMap = new HashMap<>();
-        parentGroupMap.entrySet().forEach(entry -> {
-            ArrayList<Person> pGroup = entry.getValue();
-            HashSet<String> pName = new HashSet<>();
-            for (int i=0; i< pGroup.size(); i++) {
-                pName.add(pGroup.get(i).getRecordID());
-            }
-            AtomicBoolean check = new AtomicBoolean(false);
-            AtomicReference<String> pID = new AtomicReference<>(entry.getKey());
-            nameMap.entrySet().forEach(nm -> {
-
-                ArrayList<Person> nmGroup = nm.getValue();
-                HashSet<String> nmName = new HashSet<>();
-                for (int i=0; i< nmGroup.size(); i++) {
-                    nmName.add(nmGroup.get(i).getRecordID());
-                }
-
-                if (nmName.equals(pName)) {
-                    check.set(true);
-                    pID.set(nm.getKey());
-                }
-            });
-            if (check.get() == true) {
-                nameMap.put(entry.getKey() + "," + pID, entry.getValue());
-                nameMap.remove(pID.get());
-            } else {
-                nameMap.put(entry.getKey(), entry.getValue());
-            }
-        });
-
-        nameMap.entrySet().forEach(entry -> {
-            System.out.println(entry.getKey() + "---");
-            for (int i=0; i<entry.getValue().size(); i++) {
-                entry.getValue().get(i).println();
-            }
-        });
-
+        PersonManager.combineParentIDForSameChild(parentGroupMap, nameMap);
 
         System.out.println("-------Start Exercise 1-----------");
         nameMap.entrySet().forEach(entry -> {
@@ -130,92 +58,42 @@ public class Main
                         System.out.print(", ");
                     }
                     System.out.print(name);
-
                 }
                 System.out.println("");
                 textSpace = "    ";
-
                 for (int i=0; i<entry.getValue().size(); i++) {
                     System.out.println(textSpace + entry.getValue().get(i).getName());
-                    printChildName(entry.getValue().get(i).getRecordID(), nameMap, textSpace + "    ", allPersonList);
+                    PersonManager.printChildName(entry.getValue().get(i).getRecordID(), nameMap, textSpace + "    ", allPersonList, existedList);
                 }
             }
         });
         System.out.println("-------End Exercise 1-----------");
 
         System.out.println("-------Start Exercise 2-----------");
-        ArrayList<String> exist = new ArrayList<>();
+        ArrayList<String> finalExistedList = existedList;
         topPersonList.entrySet().forEach(entry -> {
-            String textSpace = "";
-
-
-            String partnerName = findPartner(entry.getKey(), entry.getValue(), nameMap, allPersonList);
-            System.out.print(entry.getValue());
-            if (!partnerName.equals("")) {
-                System.out.println(", " + partnerName);
-            } else{
-
-                System.out.println("");
-            }
-            printChildName(entry.getKey(), nameMap, textSpace + "    ", allPersonList);
+            sortedTopPerson.add(entry.getKey());
         });
-
-        System.out.println("-------End Exercise 2-----------");
-
-    }
-
-    public static void printChildName(String id, HashMap<String, ArrayList<Person>> nameMap, String texSpace, ArrayList<Person> allPersonList) {
-
-        nameMap.entrySet().forEach(entry -> {
-            String pID[] = entry.getKey().split(",");
-            for (String token: pID) {
-                if (token.equals(id)) {
-                    ArrayList<Person> p = entry.getValue();
-                    for (int i=0; i<p.size(); i++) {
-                        System.out.print(texSpace + p.get(i).getName());
-                        String partnerName = findPartner(p.get(i).getRecordID(), p.get(i).getName(), nameMap, allPersonList);
-                        if (!partnerName.equals("")) {
-                            System.out.println(", " + partnerName);
-                        } else{
-                            System.out.println("");
+        Collections.sort(sortedTopPerson);
+        for (int i=0; i<sortedTopPerson.size(); i++) {
+            String key = sortedTopPerson.get(i);
+            topPersonList.entrySet().forEach(entry -> {
+                if (key.equals(entry.getKey())) {
+                    boolean existed = false;
+                    for (int j = 0; j< finalExistedList.size(); j++) {
+                        if (finalExistedList.get(j).equals(entry.getKey())){
+                            existed = true;
                         }
-                        printChildName(entry.getValue().get(i).getRecordID(), nameMap, texSpace + "    ", allPersonList);
+                    }
+                    if (!existed) {
+                        String textSpace = "";
+                        System.out.print(entry.getValue());
+                        PersonManager.findPartner(entry.getKey(), entry.getValue(), nameMap, allPersonList, finalExistedList);
+                        PersonManager.printChildName(entry.getKey(), nameMap, textSpace + "    ", allPersonList, finalExistedList);
                     }
                 }
-            }
-
-        });
-    }
-
-    public static String findPartner(String id, String name, HashMap<String, ArrayList<Person>> nameMap, ArrayList<Person> list) {
-
-        AtomicReference<String> partnerID = new AtomicReference<>("");
-        String partnerName = "";
-        nameMap.entrySet().forEach(entry -> {
-            AtomicBoolean check = new AtomicBoolean(false);
-            String words[] = entry.getKey().split(",");
-            for (String token: words) {
-                if (token.equals(id)) {
-                    check.set(true);
-                }
-            }
-            if (check.get() == true) {
-                for (String token: words) {
-
-                    if (!token.equals(id)) {
-                        partnerID.set(token);
-                    }
-                }
-            }
-        });
-
-        String temp = partnerID.get();
-        for (int i=0; i<list.size(); i++) {
-            if (temp.equals(list.get(i).getRecordID()) && !name.equals(list.get(i).getName())) {
-                partnerName = list.get(i).getName();
-            }
+            });
         }
-        return partnerName;
+        System.out.println("-------End Exercise 2-----------");
     }
-
 }
---
+++ src/com/razman/PersonManager.java
@@ -0,0 +1,138 @@
+package com.razman;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.concurrent.atomic.AtomicBoolean;
+import java.util.concurrent.atomic.AtomicReference;
+
+public class PersonManager {
+
+    public static void groupPersonByParentID(ArrayList<Person> allPersonList, HashMap<String, ArrayList<Person>> parentGroupMap, HashMap<String,  String> topPersonList) {
+
+        for (int i = 0; i < allPersonList.size(); i++) {
+            Person person = allPersonList.get(i);
+            boolean topParent = true;
+            for (int j = 0; j < allPersonList.size(); j++) {
+                Person list2 = allPersonList.get(j);
+                if (person.getParentID().equals(list2.getRecordID())) {
+                    if (parentGroupMap.containsKey(person.getParentID())) {
+                        ArrayList<Person> temp = parentGroupMap.get(person.getParentID());
+                        boolean samePerson = false;
+                        for (int k = 0; k < temp.size(); k++) {
+                            if (person.getName().equals(temp.get(k).getName())) {
+                                samePerson = true;
+                            }
+                        }
+                        if (samePerson == false) {
+                            temp.add(person);
+                            parentGroupMap.put(person.getParentID(), temp);
+                        }
+                    } else {
+                        ArrayList<Person> temp = new ArrayList<Person>();
+                        temp.add(person);
+                        parentGroupMap.put(person.getParentID(), temp);
+                    }
+                    topParent = false;
+                }
+            }
+            ArrayList<Person> temp = new ArrayList<Person>();
+            if (topParent) {
+                if (!parentGroupMap.containsKey(person.getParentID())) {
+                    parentGroupMap.put(person.getParentID(), temp);
+                    topPersonList.put(person.getRecordID(), person.getName());
+                }
+            }
+        }
+    }
+
+    public static void combineParentIDForSameChild(HashMap<String, ArrayList<Person>> parentGroupMap, HashMap<String, ArrayList<Person>> nameMap) {
+
+        parentGroupMap.entrySet().forEach(entry -> {
+            ArrayList<Person> pGroup = entry.getValue();
+            HashSet<String> pName = new HashSet<>();
+            for (int i=0; i< pGroup.size(); i++) {
+                pName.add(pGroup.get(i).getRecordID());
+            }
+            AtomicBoolean check = new AtomicBoolean(false);
+            AtomicReference<String> pID = new AtomicReference<>(entry.getKey());
+            nameMap.entrySet().forEach(nm -> {
+                ArrayList<Person> nmGroup = nm.getValue();
+                HashSet<String> nmName = new HashSet<>();
+                for (int i=0; i< nmGroup.size(); i++) {
+                    nmName.add(nmGroup.get(i).getRecordID());
+                }
+
+                if (nmName.equals(pName)) {
+                    check.set(true);
+                    pID.set(nm.getKey());
+                }
+            });
+            if (check.get() == true) {
+                nameMap.put(entry.getKey() + "," + pID, entry.getValue());
+                nameMap.remove(pID.get());
+            } else {
+                nameMap.put(entry.getKey(), entry.getValue());
+            }
+        });
+    }
+
+    public static void printChildName(String id, HashMap<String, ArrayList<Person>> nameMap, String texSpace, ArrayList<Person> allPersonList, ArrayList<String> existedList) {
+
+        nameMap.entrySet().forEach(entry -> {
+            String pID[] = entry.getKey().split(",");
+            for (String token: pID) {
+                if (token.equals(id)) {
+                    ArrayList<Person> p = entry.getValue();
+                    for (int i=0; i<p.size(); i++) {
+                        System.out.print(texSpace + p.get(i).getName());
+                        findPartner(p.get(i).getRecordID(), p.get(i).getName(), nameMap, allPersonList, existedList);
+                        printChildName(entry.getValue().get(i).getRecordID(), nameMap, texSpace + "    ", allPersonList, existedList);
+                    }
+                }
+            }
+        });
+    }
+
+    public static void findPartner(String id, String name, HashMap<String, ArrayList<Person>> nameMap, ArrayList<Person> list, ArrayList<String> existedList) {
+
+        AtomicReference<String> partnerID = new AtomicReference<>("");
+        String partnerName = "";
+        nameMap.entrySet().forEach(entry -> {
+            AtomicBoolean check = new AtomicBoolean(false);
+            String words[] = entry.getKey().split(",");
+            for (String token: words) {
+                if (token.equals(id)) {
+                    check.set(true);
+                }
+            }
+            if (check.get() == true) {
+                for (String token: words) {
+                    if (!token.equals(id)) {
+                        partnerID.set(token);
+                    }
+                }
+            }
+        });
+        String temp = partnerID.get();
+        boolean existed = false;
+        for (int i=0; i<existedList.size(); i++) {
+            if (existedList.get(i).equals(existed)) {
+                existed = true;
+            }
+        }
+        if (!existed) {
+            for (int i=0; i<list.size(); i++) {
+                if (temp.equals(list.get(i).getRecordID()) && !name.equals(list.get(i).getName())) {
+                    partnerName = list.get(i).getName();
+                }
+            }
+        }
+        if (!partnerName.equals("")) {
+            System.out.println(", " + partnerName);
+            existedList.add(temp);
+        } else {
+            System.out.println("");
+        }
+    }
+}
